59eb0f244c17c9a9ae2fd3358395ad39aff5cbd6
RupertGuppy03 <rupertguppy03@gmail.com>
2026-08-12 14:32:00 +1200

    Begin working on management page

--- makerspace-booking-system.client/src/main.tsx
+++ makerspace-booking-system.client/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import ManagementPage from './pages/Management/ManagementPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ManagementPage />
   </StrictMode>,
 )
